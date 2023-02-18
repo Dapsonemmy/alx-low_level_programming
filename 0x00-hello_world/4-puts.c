0eb83330da0716997bc3e04dc354a0c96c500957
Dapsonemmy <dapsonemmy@gmail.com>
2023-02-18 10:25:52 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - program to print a line
+ * main -Print program
  *
- * Return: Always 0
-*/
-
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	return (0);
 }
